74bf45101a7d367c32ffa4a8f9998d00bd81bfa4
Tim Savery <tim.savery@gmail.com>
2012-02-27 15:46:02 -0500

    added /tweets endpoint to retreive tweets back to the site.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,27 @@
+var request = require('request');
+
 exports.index = function(req, res){
   res.render('index')
+};
+
+exports.tweets = function(req, res) {
+  request.get('http://search.twitter.com/search.json?q=node.philly', function(error, response, body) {
+    var toDisplay = [];
+    
+    if (!error && response.statusCode == 200) {
+      var tweets = JSON.parse(body);
+      
+      tweets.results.forEach(function(tweet) {
+        toDisplay.push({
+          id: tweet.id,
+          user: tweet.from_user,
+          user_name: tweet.from_user_name,
+          user_profile_image: tweet.profile_image_url,
+          text: tweet.text
+        });
+      });
+    }
+    
+    res.send(toDisplay);
+  });
 };
