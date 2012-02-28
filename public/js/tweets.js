437065447333c4d179880bfc9b34004e39898df5
Tim Savery <tim.savery@gmail.com>
2012-02-28 11:37:08 -0500

    profiles,tweets,sessions

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -13,8 +13,8 @@ function loadTweets() {
       if ($('#tweets li[id="' + tweet.id + '"]').length == 0) {      
         var markup = '\
           <li id="' + tweet.id + '">\
-     	      <p class="user">' + injectUserLinks("@" + tweet.user) + '</p>\
-     	      <p class="text">' + injectUserLinks(tweet.text) + '</p>\
+     	      <p class="user">' + injectLinks("@" + tweet.user) + '</p>\
+     	      <p class="text">' + injectLinks(tweet.text) + '</p>\
        	  </li>\
         ';
 
@@ -32,6 +32,15 @@ function loadTweets() {
   setTimeout("loadTweets()", 10000);
 };
 
-function injectUserLinks(text) {
-  return text.replace(/@([^\W]*)/g, '<a href="https://www.twitter.com/#!/$1" target="_blank">@$1</a>');
+function injectLinks(text) {
+  // links
+  var result = text.replace(/([a-zA-Z]*:\/\/[a-zA-Z0-9\.\-\/]*)/g, '<a href="$1" target="_blank">$1</a>');
+
+  // users
+  result = result.replace(/@([a-zA-Z0-9_]*)/g, '<a href="http://www.twitter.com/$1" target="_blank">@$1</a>');
+
+  // hashtags
+  result = result.replace(/#([a-zA-Z0-9]*)/g, '<a href="https://twitter.com/#!/search/%23$1" target="_blank">#$1</a>');
+
+  return result;
 };
