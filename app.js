c121661ed88317d6975202934c56d156bd15fc0d
Tim Savery <tim.savery@gmail.com>
2012-02-29 10:33:48 -0500

    remove port usage

--- app.js
+++ app.js
@@ -21,4 +21,3 @@ app.get('/', routes.index);
 app.get('/tweets/since/:last', routes.tweets);
 
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
