81278b5c36b02460ce69b0d4aed1dbc7c9fed3a3
Tim Savery <tim.savery@gmail.com>
2012-03-08 09:04:24 -0500

    bretts merge

--- public/js/browser-resize.js
+++ public/js/browser-resize.js
@@ -1,5 +1,6 @@
-//swap stylesheet based on size
-// thank you css tricks for the base code instructions http://css-tricks.com/resolution-specific-stylesheets/
+//swap stylesheet based on resize
+// Author: Chatham Financial, Ted Strohecker, Brett Whitham
+
 var alreadyAppended = true;
 
 function adjustStyle(width) {
@@ -8,19 +9,26 @@ function adjustStyle(width) {
 		if(alreadyAppended){
 			$('#tweets').remove();
 			$('ul').addClass('toggle');
-			$('h1#title').addClass('toggleView').wrap('<a>');
-			$(".toggle").hide();			
+			$('h1#title').addClass('toggleView').wrap('<a class="mobileSwitch">');
+			$('h1#title').addClass('closed');
+			$(".toggle").hide();
+			$(".tweet-head").html('<img src="/images/mobile-twitter-bird.png" class="twitter-bird"/>');
+			$("#sponsors").insertAfter("section.speakers");
+			$('.logo').html("Node.Philly 2012");		
 		alreadyAppended = false;
 		}
     }
 	else {
 		if(!alreadyAppended){
+			$("#sponsors").prependTo(".sidebar");			
 			$('.sidebar').append('<ul id="tweets"></ul>');
 			$("ul").show();
 			$('ul').removeClass('toggle');
 			$('h1#title').removeClass('toggleView');
 			$('h1#title').unwrap();
-			alreadyAppended = true;
+			$(".tweet-head").html("Follow @NodePhilly");
+			$('.logo').html("&nbsp;");		
+		alreadyAppended = true;
 		}		
 	}
 }
@@ -33,11 +41,26 @@ $(function() {
 });
 
 $(document).ready(function(){
-	$('h1#title').addClass('toggleView').wrap('<a>');
+	$('h1#title').addClass('toggleView').wrap('<a class="mobileSwitch">');
 	$('a h1.toggleView').click(function() {
-    		$(this).parent().next('.toggle').slideToggle(250);
+			setupClickEvent($(this));
+    		$(this).parent().next('.toggle').slideToggle(500)
   	      });	
  	$('h1#title').removeClass('toggleView');
 	$('h1#title').unwrap();
 	
 });
+function setupClickEvent(element){
+	var openOrClosed = element.attr('class');
+    if (openOrClosed == 'closed') {
+		element.removeClass('closed');
+        element.addClass('open');
+		var positionY = element.position().top;
+		$('body').animate( { scrollTop: positionY}, 500);		
+
+    }
+    else {
+		element.removeClass('open');
+        element.addClass('closed');
+    }
+}
